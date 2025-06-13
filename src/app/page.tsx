5f9e21ceb65832e0674c1e4f6ca8f218ade8e412
Bonolo Nthejane <bknthejane@gmail.com>
2025-06-13 10:13:37 +0200

    Modified file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 export default function Home() {
 
-  const user = JSON.parse(localStorage.getItem("UserInfo") || "");
-  console.log(user)
+  /* const user = JSON.parse(localStorage.getItem("UserInfo") || "");
+  console.log(user) */
 
   return (
     <>
       <h1>Homepage</h1>
-      <h1>{user?.username}</h1>
+      {/* <h1>{user?.username}</h1> */}
     </>
   );
 }
